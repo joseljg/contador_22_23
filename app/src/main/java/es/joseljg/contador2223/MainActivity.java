2dfad08e4fad9aae1c8015cd4e6ed904c6d70ef4
joseljg <joseljg@gmail.com>
2022-09-21 19:20:25 +0200

    version1.1

--- app/src/main/java/es/joseljg/contador2223/MainActivity.java
+++ app/src/main/java/es/joseljg/contador2223/MainActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int cont; // creas una variable contador
+    TextView txt_contador = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cont = 0;
+        // hay que enlazar la variable txt_contador con la pantalla->view
+        txt_contador = findViewById(R.id.txt_contador);
     }
 
     public void mostrarMensaje(View view) {
@@ -19,12 +25,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void contarNumeros(View view) {
-
+       cont++;
+       // volver a pintar txt_contador
+       txt_contador.setText(String.valueOf(cont));
     }
 
 
     public void resetearContador(View view) {
-
+      cont = 0;
+      txt_contador.setText(String.valueOf(cont));
     }
 
 }
